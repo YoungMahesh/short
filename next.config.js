e5f9ba4dfbb98f405a9591e5a651afbc448bf03b
Mahesh Ghamand <maheshghamand@gmail.com>
2020-12-19 16:05:12 +0530

    node-version is defined in package.json

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
 	env: {
-		BASE_URL: process.env.NODE_ENV === 'development' ? 'localhost:3000' : 'in.now.sh'
+		BASE_URL: process.env.NODE_ENV === 'development' ? 'localhost:3000' : 'in.now.sh',
+		FAUNADB_COLLECTION: 'url-shortner',
+		FAUNADB_INDEX: 'short_urls'
 	}
 }
